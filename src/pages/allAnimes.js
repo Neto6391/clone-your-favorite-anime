91abad214f0cac63b76821a11407985ddbc9c91e
nettorodrigues <neto6391@gmail.com>
2021-10-17 23:50:30 -0300

    feat(show-animes-page): refactor page to call a list of components

--- src/pages/allAnimes.js
+++ src/pages/allAnimes.js
@@ -1,76 +1,21 @@
-import { ButtonBase, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, IconButton, makeStyles, Typography } from "@material-ui/core";
+import AnimeList from "../components/animes/AnimeList";
 import Layout from "../components/layout/Layout";
-import FavoriteIcon from '@material-ui/icons/Favorite';
 
-const useStyles = makeStyles({
-    sizeCard: {
-        maxWidth: '240px'
-    },
-    marginCard: {
-        margin: '12px'
-    },
-
-    iconButtonFavorite: {
-        top: 0,
-        right: 0,
-        position: 'absolute'
-    },
-
-    iconFavorite: {
-        color: '#e0e0e0 !important', //quando ativar '#ff5252'
-        padding: '8px',
-        borderRadius: '100px',
-        backgroundColor: 'rgba(255, 0, 0, 0.7) !important', //quando ativar rgba(0, 0, 0, 0.7)'
-        "& :hover": {
-            color: '#ff5252 !important', // quando desativar #e0e0e0
-            backgroundColor: 'rgba(0, 0, 0, 0.7) !important' // quando desativar 'rgba(255, 0, 0, 0.7)'
-        }
-    },
-
-    hoverButtonEffect: {
-        ":hover": {
-            color: '#e0e0e0 !important',
-            backgroundColor: 'rgba(0, 0, 0, 0.9) !important' // quando desativar 'rgba(255, 0, 0, 0.7)'
-        }
-    }
-
-
-    
-
-  });
 
 function AllAnimesPage() {
-    const classes = useStyles();
+    const animes = [
+        { labelName: 'Cowboy Bepop1' },
+        { labelName: 'Cowboy Bepop2' },
+        { labelName: 'Cowboy Bepop3' },
+        { labelName: 'Cowboy Bepop4' },
+        { labelName: 'Cowboy Bepop5' },
+        { labelName: 'Cowboy Bepop6' },
+        { labelName: 'Cowboy Bepop7' },
+        { labelName: 'Cowboy Bepop8' }
+    ];
     return (
         <Layout>
-            <Grid container 
-                direction="column"
-                alignItems="center"
-                spacing={0}
-            >
-                <Grid item className={classes.marginCard}>
-                    <Card className={classes.sizeCard}>
-                        <ButtonBase >
-                            <CardActionArea>
-                                <CardMedia className={classes.hoverButtonEffect}
-                                        component="img"
-                                        height="340"
-                                        image="https://media.kitsu.io/anime/poster_images/1/large.jpg"
-                                        alt="green iguana"
-                                    />
-                                <IconButton className={classes.iconButtonFavorite}>
-                                    <FavoriteIcon className={classes.iconFavorite} fontSize="large" />
-                                </IconButton>
-                                <Typography variant="body1" color="text.secondary">
-                                    Cowboy Bebop
-                                </Typography>
-                            </CardActionArea>
-                             
-                           
-                        </ButtonBase>
-                    </Card>
-                </Grid>
-            </Grid>
+            <AnimeList animes={animes} />
         </Layout>
     );
 }
