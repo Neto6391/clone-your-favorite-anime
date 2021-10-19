ee4b9cf70196ff4b99d80bb35cdf8c54e8aacbd9
nettorodrigues <neto6391@gmail.com>
2021-10-19 13:26:41 -0300

    feat(show-animes-page): refactor anime item

--- src/components/animes/AnimeItem.js
+++ src/components/animes/AnimeItem.js
@@ -1,5 +1,6 @@
 import {  Card, CardActionArea, CardMedia, Grid, IconButton, makeStyles, Typography } from "@material-ui/core";
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import { useEffect, useState } from "react";
 
 const useStyles = makeStyles({
     sizeCard: {
@@ -13,51 +14,78 @@ const useStyles = makeStyles({
     iconButtonFavorite: {
         top: 15,
         right: 45,
-        position: 'absolute'
+        position: 'absolute',
+       
     },
 
-    iconFavorite: {
+    iconFavoriteHovered: {
         color: '#e0e0e0 !important', //quando ativar '#ff5252'
         padding: '8px',
         borderRadius: '100px',
-        backgroundColor: 'rgba(255, 0, 0, 0.7) !important', //quando ativar rgba(0, 0, 0, 0.7)'
-        "& :hover": {
-            color: '#ff5252 !important', // quando desativar #e0e0e0
-            backgroundColor: 'rgba(0, 0, 0, 0.7) !important' // quando desativar 'rgba(255, 0, 0, 0.7)'
-        }
+        backgroundColor: 'rgba(0, 0, 0, 0.7) !important',
+        color: '#ff5252 !important'
     },
 
-    hoverButtonEffect: {
-        ":hover": {
-            color: '#e0e0e0 !important',
-            backgroundColor: 'rgba(0, 0, 0, 0.9) !important' // quando desativar 'rgba(255, 0, 0, 0.7)'
+    iconFavoriteNoHovered: {
+        color: '#e0e0e0 !important',
+        padding: '8px',
+        borderRadius: '100px',
+        backgroundColor: 'rgba(255, 0, 0, 0.7) !important',
+        color: '#e0e0e0 !important',
+    },
+
+    media: {
+        height: 140
+      },
+    action: {
+        position: "relative",
+        "&:hover": {
+            filter: 'contrast(170%)'
         }
     }
-  });
+    
+});
+
+
 
 function AnimeItem(props) {
     const classes = useStyles();
 
+    const [isButtonHovered, setHoveredButton] = useState(false);
+
+    useEffect(() => {
+        if (props.favorite) {
+            setHoveredButton(true);
+        }
+    }, []);
+
     return (
         <Grid item className={classes.marginCard}>
-                        <Card className={classes.sizeCard}>
-                            <CardActionArea onClick={() => console.log('teste!')}>
+                        <Card className={classes.sizeCard, classes.action}>
+                            <CardActionArea 
+                                onClick={() => console.log('teste!')}
+                            >
                             <CardMedia 
                                 component="img"
                                 height="340"
-                                image="https://media.kitsu.io/anime/poster_images/1/large.jpg"
+                                image={props.image}
                                 alt={ props.titleCard }
                             />
                                 
-                                    
-                            
                             <Typography variant="body1" align="center" color="textPrimary">
                                 { props.titleCard }
                             </Typography>
                             </CardActionArea>                             
-                            <IconButton className={classes.iconButtonFavorite} size="small">
-                                <FavoriteIcon className={classes.iconFavorite}  fontSize="large" />
-                            </IconButton>
+                                <IconButton 
+                                    className={classes.iconButtonFavorite} 
+                                    size="small" 
+                                    onMouseOver={()=> isButtonHovered ? setHoveredButton(false) : setHoveredButton(true) }
+                                    onMouseOut={() => isButtonHovered ? setHoveredButton(false) : setHoveredButton(true) }
+                                    // onClick={}
+                                > 
+                                    { isButtonHovered ? <FavoriteIcon className={classes.iconFavoriteHovered}  fontSize="large" /> : <FavoriteIcon className={classes.iconFavoriteNoHovered}  fontSize="large" /> } 
+                                </IconButton>
+                                
                         </Card>
         </Grid>
     );
